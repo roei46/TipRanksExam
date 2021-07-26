5db7d6fea54e0e9b247c860b4604a6cbd43d96c3
Roei Baruch <roei.baruch.pepper@gmail.com>
2021-07-26 19:01:56 +0300

    Changed extension to states

--- TipRanksExam/Extension/UITableView + extention.swift
+++ TipRanksExam/Extension/UITableView + extention.swift
@@ -11,15 +11,24 @@ import RxSwift
 import RxCocoa
 
 extension Reactive where Base: UITableView {
-    public var animation: Binder<Bool> {
-        return Binder(self.base) { hud, show in
+    var animation: Binder<NetworkState> {
+        return Binder(self.base) { hud, state in
             let spinner = UIActivityIndicatorView(style: .medium)
             spinner.startAnimating()
             spinner.frame = CGRect(x: CGFloat(0), y: CGFloat(0), width: hud.bounds.width, height: CGFloat(44))
-            hud.tableFooterView = spinner
-            if show {
+            switch state {
+            case .footer:
+                let footerView = UIView(frame: CGRect(x: 0, y: 0, width: hud.frame.size.width, height: 40))
+                footerView.backgroundColor = .red
+                let labelFooter = UILabel(frame: footerView.frame)
+                labelFooter.text = " End of the list! "
+                labelFooter.textColor = .black
+                footerView.addSubview(labelFooter)
+                hud.tableFooterView = footerView
+            case .loading:
+                hud.tableFooterView = spinner
                 hud.tableFooterView?.isHidden = true
-            } else {
+            case .notLoading:
                 hud.tableFooterView?.isHidden = false
                 hud.reloadData()
             }
